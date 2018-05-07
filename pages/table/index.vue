ffd2ccb11bca92148d6cc6a61371aa03ff57d735
raul <raultrysw@gmail.com>
2018-05-07 18:07:53 +0200

    Hecho el esqueleto de los tres modos

---
+++ pages/table/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul>
+      <li><a href="/table/edit">Edicion</a></li>
+      <li><a href="/table/study">Estudio</a></li>
+      <li><a href="/table/test">Examen</a></li>
+    </ul>
+    <nuxt-child />
+  </div>
+</template>
---
+++ pages/table/index/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>El modo es examen</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    mode() {
+      return this.$route.params.mode
+    }
+  }
+}
+</script>
